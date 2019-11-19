7dcc1739dd5d21be5bcbb9a26f3df2200f5803d3
Victor <vctr90w@gmail.com>
2019-11-19 00:51:26 -0400

    Added redux functionality for category selection

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
 export const ADD_OUTCOME = 'ADD_OUTCOME';
 export const ADD_INCOME = 'ADD_INCOME';
+export const CATEGORY_CHANGE = 'CATEGORY_CHANGE'
 
 export const addOutcome = expense => ({
   type: ADD_OUTCOME,
@@ -10,3 +11,8 @@ export const addIncome = income => ({
   type: ADD_INCOME,
   payload: income
 });
+
+export const categoryChange = categoryValue => ({
+  type: CATEGORY_CHANGE,
+  payload: categoryValue
+})
--- src/redux/reducers/expensesManagerReducer.js
+++ src/redux/reducers/expensesManagerReducer.js
@@ -1,18 +1,23 @@
 import _ from 'lodash';
 
-import { ADD_OUTCOME, ADD_INCOME, addIncome } from '../actions/index';
+import { ADD_OUTCOME, ADD_INCOME, CATEGORY_CHANGE } from '../actions/index';
 
 const initialState = {
   entries: {
     incomes: [],
     outcomes: []
-  }
+  },
+  category: ''
 }
 
 const addEntry = ({ entry, entryType, state }) => {
   const entriesToInsert = [...state.entries[entryType], entry];
   const newState = _.chain({}).merge(state).merge({entries: {[entryType]: entriesToInsert}}).value();
   return newState;
+};
+
+const categoryChange = ({ categoryValue, currentState}) => {
+  return { ...currentState, category: categoryValue };
 };
 
 export const reducer = (state = initialState, action) => {
@@ -28,6 +33,7 @@ export const reducer = (state = initialState, action) => {
         entryType: 'incomes',
         state
       });
+    case CATEGORY_CHANGE: return categoryChange({ categoryValue: payload, currentState: state });
     default:
       return state;
   }
