a535e4f6d9b3122423f7a80360e09daa40c7b5f9
Victor <vctr90w@gmail.com>
2019-12-01 13:37:06 -0400

    Added first step for abstraction

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import _ from 'lodash';
 import Results from './Results';
 import AddEntry from './AddEntry';
 import Summary from './Summary';
