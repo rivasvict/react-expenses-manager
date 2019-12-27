01557ca371929ef82eefab5aa8652234909b5699
Victor <vctr90w@gmail.com>
2019-12-27 02:56:12 -0400

    Added a way to communicate ith the server

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,6 +3,7 @@ export const ADD_INCOME = 'ADD_INCOME';
 export const CATEGORY_CHANGE = 'CATEGORY_CHANGE'
 export const IS_LOADING = 'IS_LOADING';
 export const HAS_USER_BEEN_CREATED = 'HAS_USER_BEEN_CREATED ';
+export const HAS_ERRORED = 'HAS_ERRORED';
 
 export const addOutcome = expense => ({
   type: ADD_OUTCOME, payload: expense
@@ -24,23 +25,36 @@ export const isLoading = isLoadingHappening => ({
 })
 
 export const hasErrored = error => ({
-  type: IS_LOADING,
+  type: HAS_ERRORED,
   payload: error
 })
 
-export const hasUserBeenCreated = () => ({
+// TODO: Choose a better action name
+export const hasUserBeenCreated = user => ({
   type: HAS_USER_BEEN_CREATED,
-  payload: true
+  payload: user
 });
 
+// TODO: Handle loading state
 export const createUser = (userPayload) => {
   return async (dispatch, getState) => {
-    const baseUrl = process.env.REACT_APP_API_URL;
-    const url = `${baseUrl}/api/user/sign-up`;
-    const response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(userPayload)
-    })
-    debugger;
+    try {
+      const baseUrl = process.env.REACT_APP_API_URL;
+      const url = `${baseUrl}/api/user/sign-up`;
+      const body = JSON.stringify({ user: userPayload });
+      const rawResponse = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body
+      });
+      const response = await rawResponse.json()
+      if (!rawResponse.ok) {
+        throw new Error(response.message)
+      }
+
+      dispatch(hasUserBeenCreated(userPayload));
+    } catch (error) {
+      dispatch(hasErrored(error));
+    }
   }
 }
