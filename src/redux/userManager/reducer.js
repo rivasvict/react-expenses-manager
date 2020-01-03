99fb892ba44c1071a87228ea0e342a43676750f7
Victor <vctr90w@gmail.com>
2020-01-03 17:21:10 -0400

    Added a better way to handle errors for user creation

--- src/redux/userManager/reducer.js
+++ src/redux/userManager/reducer.js
@@ -1,13 +1,27 @@
 import { CREATE_USER_ERROR, CREATE_USER_SUCCESS, CREATE_USER_LOADING } from "./actions";
-const defaultState = { user: {}, validationErrors: { validation: []}, isLoading: false }
+const defaultState = { user: {}, validationErrors: { validation: [] }, isLoading: false, error: null }
+
+const handleError = (error, state) => {
+  if (error && error.name === 'ValidationError') {
+    return {
+      ...state,
+      validationErrors: {
+        validation: error.validation
+      }
+    }
+  }
+
+  return {
+    ...state,
+    error
+  }
+};
 
 export const reducer = (state = defaultState, action) => {
   const { type, payload } = action;
+
   switch (type) {
-    case CREATE_USER_ERROR: return {
-      ...state,
-      validationErrors: payload
-    };
+    case CREATE_USER_ERROR: return handleError(payload, state);
     case CREATE_USER_SUCCESS: return {
       ...state,
       user: payload.user
