0e12f87a9abcae9c0d58516fbb63f6daa6b169d0
Victor <vctr90w@gmail.com>
2020-01-08 23:31:41 -0400

    First approach to validation

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createUser } from '../../redux/userManager/actoionCreators';
 import { useHistory } from 'react-router-dom';
+import _ from 'lodash';
 
 function SignUp(props) {
 
@@ -16,8 +17,7 @@ function SignUp(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    props.onCreateUser(formState);
-    history.push('/');
+    props.onCreateUser(_.omit(formState, 'retype-password'));
   };
 
   function handleChange(event) {
@@ -35,12 +35,16 @@ function SignUp(props) {
 
   return (
     <form>
-      <label>First Name: </label><input type='text' name='firstName' placeholder='First Name goes here'  onChange={handleChange}></input><br />
-      {props.validationErrors.find(validationError => validationError.path === 'firstName') ? <label>First name is required</label> : null}
-      <label>Last Name: </label><input type='text' name='lastName' placeholder='Last Name goes here'  onChange={handleChange}></input><br />
-      <label>Email: </label><input type='text' name='email' placeholder='Your email goes here'  onChange={handleChange}></input><br />
-      <label>Password: </label><input type='password' name='password' placeholder='Type password'  onChange={handleChange}></input><br />
-      {props.isLoading ? 'loading...' : <button type='submit' onClick={handleSubmit}>Submit</button>}
+      <label>First Name: </label><input type='text' name='firstName' placeholder='First Name goes here'  onChange={handleChange}></input>
+      {props.validationErrors.find(validationError => validationError.path === 'firstName') ? <React.Fragment><br /><label>First name is required</label></React.Fragment> : null}
+      <br /><label>Last Name: </label><input type='text' name='lastName' placeholder='Last Name goes here'  onChange={handleChange}></input>
+      {props.validationErrors.find(validationError => validationError.path === 'lastName') ? <React.Fragment><br /><label>Last name is required</label></React.Fragment> : null}
+      <br /><label>Email: </label><input type='text' name='email' placeholder='Your email goes here'  onChange={handleChange}></input>
+      {props.validationErrors.find(validationError => validationError.path === 'email') ? <React.Fragment><br /><label>Email is required</label></React.Fragment> : null}
+      <br /><label>Password: </label><input type='password' name='password' placeholder='Type password'  onChange={handleChange}></input>
+      {props.validationErrors.find(validationError => validationError.path === 'password') ? <React.Fragment><br /><label>Password is required</label></React.Fragment> : null}
+      <br /><label>Retype password: </label><input type='password' name='password-retype' placeholder='Type password'  onChange={handleChange}></input>
+      <br />{props.isLoading ? 'loading...' : <button type='submit' onClick={handleSubmit}>Submit</button>}
       <button onClick={handleCancel}>Cancel</button>
     </form>
   )
