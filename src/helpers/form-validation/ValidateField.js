765cc7a1c3356c918500816b29e77b2e6385f56a
Victor <vctr90w@gmail.com>
2020-01-10 00:34:17 -0400

    first approach to validation

---
+++ src/helpers/form-validation/ValidateField.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const defaultValidationRules = {
+  required: ({ value, fieldName, message }) => () => {
+    const validationMessage = message || `${fieldName} is required, please insert it`;
+    return !value.length ? validationMessage : '';
+  },
+  email: ({ value, fieldName, message }) => () => {
+    const validationMessage = message || `${fieldName} has a wrong email format`;
+    const emailPattern = /@/g;
+    return value.match(emailPattern) ? validationMessage : '';
+  }
+};
+
+function ValidateField(props) {
+  const { validationTypes = [], isFormValid = false, customValidations = [], value = '' } = props;
+  const buildInValidations = validationTypes.map(validationType => {
+    return defaultValidationRules[validationType.name]({ value, fieldName: props.children.props.name, message: validationType.message })
+  });
+  const fullValidations = [...buildInValidations, ...customValidations];
+  const validationMessages = fullValidations.map(singleValidation => singleValidation());
+  const isFieldValid = validationMessages.every(singleValidation => singleValidation === '');
+  const validationMessagesWithTemplate = validationMessages.map((validationMessage, index) => <label key={index}>{validationMessage}</label>)
+
+  return (
+    <div>{props.children}{isFieldValid ? null : validationMessagesWithTemplate}</div>
+  )
+}
+
+export default ValidateField;
