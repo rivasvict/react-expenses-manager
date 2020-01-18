532f0d0d7586171522e71d6a05ffecc7987ad129
Victor <vctr90w@gmail.com>
2020-01-18 16:00:49 -0400

    Added a nicer html for when no validation has been set to the field

--- src/helpers/form-validation/ValidateField.js
+++ src/helpers/form-validation/ValidateField.js
@@ -4,18 +4,24 @@ import { ValidationContext } from './FormValidation';
 
 function ValidateField(props) {
   const { formState } = useContext(ValidationContext);
-  const childrenProps = props.children.props;
-  const fieldName = childrenProps.name;
-  const { validation, values } = formState;
-  const validationTypes = validation[childrenProps.name].buildInValidations;
-  const customValidations = validation[childrenProps.name].customValidations;
-  const value = values[fieldName];
-  const { validationMessages, isFieldValid } = getValidationForField({ validationTypes, customValidations, value, fieldName });
-  const validationMessagesWithTemplate = validationMessages.map((validationMessage, index) => <label key={index}>{validationMessage}</label>)
+  try {
+    const childrenProps = props.children.props;
+    const fieldName = childrenProps.name;
+    const { validation, values } = formState;
+    const validationTypes = validation[childrenProps.name].buildInValidations;
+    const customValidations = validation[childrenProps.name].customValidations;
+    const value = values[fieldName];
+    const { validationMessages, isFieldValid } = getValidationForField({ validationTypes, customValidations, value, fieldName });
+    const validationMessagesWithTemplate = validationMessages.map((validationMessage, index) => <label key={index}>{validationMessage}</label>)
 
-  return (
-    <div>{props.children}{isFieldValid ? null : validationMessagesWithTemplate}</div>
-  )
+    return (
+      <div>{props.children}{isFieldValid ? null : validationMessagesWithTemplate}</div>
+    )
+  } catch(error) {
+    return (
+      <div>ERROR: Validation component cannot work without validations defined for its model</div>
+    )
+  }
 }
 
 export default ValidateField;
