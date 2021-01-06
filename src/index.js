6f41045c94064f5480ae3ce33d797cea39b75f31
Victor <vctr90w@gmail.com>
2021-01-05 21:22:57 -0500

    Added node-sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import { setupStore } from './redux/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
