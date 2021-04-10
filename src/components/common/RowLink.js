dcadb14d8eac7a73eff7445e8aeb6ff6c7c80c76
Victor <vctr90w@gmail.com>
2021-04-10 15:46:00 -0400

    Added a comment to the new RowLink component

--- src/components/common/RowLink.js
+++ src/components/common/RowLink.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+/**
+ * A custom composed implementation of bootstrap row + react router link
+ */
 function RowLink(props) {
   return (
     <Link {...{ ...props, className: `${props.className} row` }}>
