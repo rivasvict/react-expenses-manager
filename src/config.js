9b9b44b5f8b1698f7956b3a99e6cc4240ab7f01d
Victor <vctr90w@gmail.com>
2022-01-06 22:08:20 -0500

    Changed the configuration for the enviroment variable for the frontend

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-const REACT_APP_API_HOST = process.env.REACT_APP_API_HOST || 'http://localhost';
-const REACT_APP_API_PORT = process.env.REACT_APP_API_PORT || '9000';
+const REACT_APP_API_HOST = process.env.REACT_APP_API_HOST || 'http://localhost:9000';
 
 export const config = {
-  REACT_APP_API_HOST,
-  REACT_APP_API_PORT
+  REACT_APP_API_HOST
 };
