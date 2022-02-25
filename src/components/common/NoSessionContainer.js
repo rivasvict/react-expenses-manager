d0e465958dd0de13b3086aac403ce96dafa4810b
Victor Rivas <vctr90w@gmail.com>
2022-02-25 07:38:10 -0500

    Added better styles for the sign up and sign in

--- src/components/common/NoSessionContainer.js
+++ src/components/common/NoSessionContainer.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import './NoSessionContainer.scss';
 import logoImage from '../../images/expenses_tracker_logo.png';
 
 function NoSessionContainer({ children }) {
   return (
-    <Container className='NoSessionContainer'>
-      <img src={logoImage} alt='Expenses tracker logo' className='logo'/>
-      {children}
+    <Container>
+      <Row className='NoSessionContainer'>
+        <Col className='upperSide'>
+          <img src={logoImage} alt='Expenses tracker logo' className='logo'/>
+        </Col>
+        <Col>
+          {children}
+        </Col>
+      </Row>
     </Container>
   )
 };
